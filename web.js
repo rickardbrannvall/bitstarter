8a5d747d83b25fdcc6a84bac86f3190bd0d323f5
Rickard Brannvall <Rickard.Brannvall@gmail.com>
2013-07-11 23:36:08 +0000

    hw3-1 try 1

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var bf = fs.readFileSync('index.html');
+  response.send(bf.toString());
 });
 
 var port = process.env.PORT || 5000;
